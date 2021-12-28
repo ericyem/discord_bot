93f996cb5b3a660bb3448ae9bbd04cdf9f1b55b1
Eric <eyem0002@student.monsah.edu>
2021-12-28 12:40:02 +1100

    restart pipeline

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ intents = discord.Intents.default()
 intents.members = True
 bot_token = get_local_env("id.env")
 botClient = Ogikubot(command_prefix='$', intents=intents)
+# runs the bot
 botClient.run(bot_token)
